7dd6fc4c9dc1414a2a0b359e439b3d971d154bb9
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2022-05-08 19:04:00 +0800

    bump to 1.1.0

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+ rm -f ./install.zip
  zip install.zip common/* META-INF/* system/vendor/etc/* customize.sh LICENSE module.prop README.md uninstall.sh
  git add install.zip update.json changelog.md module.prop
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,21 +1,25 @@
 #!/system/bin/sh
+# define local
+GPSCONFVENDOR=/system/vendor/etc/gps.conf
+GPSCONFMODIR=$MODIR$GPSCONFVENDOR
+
 # set ownership
 chown -R 0:0 $GPSCONFVENDOR
 
 [ "$(getenforce)" = "Enforcing" ] || exit 0
 
-GPSCONFVENDOR=/system/vendor/etc/gps.conf
-
+# default se context for gps conf
 default_selinux_context=u:object_r:vendor_configs_file:s0
 selinux_context=
 
-# Detect the GPS Config is located.
+# get the original se context if present
 if [[ -f $GPSCONFVENDOR ]];then
     selinux_context=$(ls -Zd $GPSCONFVENDOR | awk '{print $1}')
 fi
 
+# set proper se context
 if [ -n "$selinux_context" ] && [ "$selinux_context" != "?" ]; then
-    chcon -R $selinux_context $GPSCONFVENDOR
+    chcon -R $selinux_context $GPSCONFMODIR
 else
-    chcon -R $default_selinux_context $GPSCONFVENDOR
+    chcon -R $default_selinux_context $GPSCONFMODIR
 fi
