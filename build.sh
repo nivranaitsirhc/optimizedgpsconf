19b10d22b718099e48e02fc4a3c4e2a527cd679b
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2022-05-08 19:06:52 +0800

    updated build.sh to a proper script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
- rm -f ./install.zip
- zip install.zip common/* META-INF/* system/vendor/etc/* customize.sh LICENSE module.prop README.md uninstall.sh
- git add install.zip update.json changelog.md module.prop
+#!/bin/bash
+rm -f ./install.zip
+zip install.zip common/* META-INF/* system/vendor/etc/* customize.sh LICENSE module.prop README.md uninstall.sh
+git add install.zip update.json changelog.md module.prop
